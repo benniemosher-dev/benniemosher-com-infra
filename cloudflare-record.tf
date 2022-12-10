7e1d923cb8b2bad36e6a9b3f33ba4c2f6349bc55
Bennie Mosher <337539+benniemosher@users.noreply.github.com>
2022-12-10 15:02:18 -0700

    BAM: Cleaned up terraform

--- cloudflare-record.tf
+++ cloudflare-record.tf
@@ -1,15 +1,10 @@
 resource "cloudflare_record" "this" {
-  for_each = merge([
-    for zone in local.cloudflare-zones : {
-      for record in zone.dns-records :
-      "${record.name}-${record.value}" => merge(record, { zone-id = cloudflare_zone.this[zone.name].id })
-    }
-  ]...)
+  for_each = { for record in local.dns-records : "${record.name}-${record.value}" => record }
 
-  zone_id  = each.value.zone-id
   name     = each.value.name
   priority = try(each.value.priority, null)
+  proxied  = try(each.value.proxied, true)
   type     = try(each.value.type, "CNAME")
   value    = each.value.value
-  proxied  = try(each.value.proxied, true)
+  zone_id  = data.cloudflare_zone.zone.id
 }
--- locals.tf
+++ locals.tf
@@ -1,50 +1,45 @@
 locals {
-  cloudflare-zones = [
+  dns-records = [
     {
-      name = "benniemosher.dev"
-      dns-records = [
-        {
-          name  = "_domainconnect"
-          value = "connect.domains.google.com"
-        },
-        {
-          name  = "www"
-          value = "benniemosher.dev"
-        },
-        {
-          name    = "_github-challenge-benniemosher-dev-org"
-          proxied = false
-          type    = "TXT"
-          value   = "49eb4ab98b"
-        },
-        {
-          name     = "benniemosher.dev"
-          priority = 69
-          type     = "MX"
-          value    = "route1.mx.cloudflare.net"
-          proxied  = false
-        },
-        {
-          name     = "benniemosher.dev"
-          priority = 96
-          type     = "MX"
-          value    = "route2.mx.cloudflare.net"
-          proxied  = false
-        },
-        {
-          name     = "benniemosher.dev"
-          priority = 25
-          type     = "MX"
-          value    = "route3.mx.cloudflare.net"
-          proxied  = false
-        },
-        {
-          name    = "benniemosher.dev"
-          type    = "TXT"
-          value   = "v=spf1 include:_spf.mx.cloudflare.net ~all"
-          proxied = false
-        }
-      ]
+      name  = "_domainconnect"
+      value = "connect.domains.google.com"
+    },
+    {
+      name  = "www"
+      value = "benniemosher.dev"
+    },
+    {
+      name    = "_github-challenge-benniemosher-dev-org"
+      proxied = false
+      type    = "TXT"
+      value   = "49eb4ab98b"
+    },
+    {
+      name     = "benniemosher.dev"
+      priority = 69
+      type     = "MX"
+      value    = "route1.mx.cloudflare.net"
+      proxied  = false
+    },
+    {
+      name     = "benniemosher.dev"
+      priority = 96
+      type     = "MX"
+      value    = "route2.mx.cloudflare.net"
+      proxied  = false
+    },
+    {
+      name     = "benniemosher.dev"
+      priority = 25
+      type     = "MX"
+      value    = "route3.mx.cloudflare.net"
+      proxied  = false
+    },
+    {
+      name    = "benniemosher.dev"
+      type    = "TXT"
+      value   = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+      proxied = false
     }
   ]
 }
